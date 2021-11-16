fe8411d7d367fe4294d3eae5394cd995f1963c92
Joao <joaovictorcgjvcg@gmail.com>
2021-11-16 15:31:33 -0400

    Entrega requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const text = () => 'Eita pessoa boa!';
+  return {
+    name,
+    feedback: text,
+  }
+};
 
 module.exports = createStudent;
