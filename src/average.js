d66333692dd26b87a1eaff42d5089ebbc3d7e79c
Joao <joaovictorcgjvcg@gmail.com>
2021-11-22 23:46:52 -0400

    entrga requisito 1 com correcao do lint

--- src/average.js
+++ src/average.js
@@ -19,8 +19,8 @@ const average = (array) => {
   if (array.length === 0) {
     return undefined;
   }
-  for (let index = 0; index < array.length; index =+ 1) {
-    if (typeof array[index] !== 'number') {
+  for (let index = 0; index < array.length; index = +1) {
+    if (typeof array[index] !== "number") {
       return undefined;
     }
     soma += array[index];
